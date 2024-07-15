1b5f2761612082ec48652c033ced11cf82403800
evergreen3579 <evergreen3579@naver.com>
2024-07-15 17:38:22 +0900

    240715_llast

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
 # app.py
 
-from flask import Flask
-from app.views import app as app_blueprint
+from flask import Flask, render_template
 
 app = Flask(__name__)
-app.secret_key = 'your_secret_key'  # 필요한 경우 플라스크 애플리케이션에 시크릿 키 설정
 
-# 블루프린트 등록
-app.register_blueprint(app_blueprint)
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
+
