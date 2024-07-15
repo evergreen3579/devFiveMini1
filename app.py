91a8fa427b20b2b2486ff0dc8ceed6a476d03e9d
evergreen3579 <evergreen3579@naver.com>
2024-07-15 14:20:11 +0900

    240715

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+# 첫 화면을 index.html로 렌더링하는 예제
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True, port=3000)
