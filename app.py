04a75093b0aaf891eaccb6dde8fe95643d22daaf
evergreen3579 <evergreen3579@naver.com>
2024-07-16 16:06:21 +0900

    240716_2

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
 # app.py
 
 from flask import Flask, render_template
+from app.views import app as app_blueprint
 
 app = Flask(__name__)
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'  # 임의의 시크릿 키 설정
+
+# Blueprint 등록
+app.register_blueprint(app_blueprint)
 
 @app.route('/')
 def index():
@@ -14,4 +19,3 @@ def signup():
 
 if __name__ == '__main__':
     app.run(debug=True)
-
